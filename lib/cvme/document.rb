3ee8a8c261ff4e13167c5452b71a923f40fa26fb
Jon <jyorg@outlook.com>
2015-09-30 02:13:11 -0700

    Refactor document.rb

--- lib/cvme/document.rb
+++ lib/cvme/document.rb
@@ -14,27 +14,33 @@ module Cvme
     def method_missing(name, *params, &block)
       param = params.first
       case name
-      when /header/ then block.call
-      when /group/  then create_group(param); block.call
-      when /entry/  then create_entry(param); block.call
-      else;  create_header(name, param) || create_entry_item(name,param)
+      when /header/ 
+        block.call
+      when /group/  
+        create_group(param)
+        block.call
+      when /entry/  
+        create_entry(param)
+        block.call
+      else
+        create_header(name, param) || create_entry_item(name,param)
       end
     end
 
     private
 
     def create_group(param)
-      group = Group.new param
-      @groups << group
+      @groups << Group.new(param)
     end
 
     def create_entry(param)
-      entry = Entry.new(param)
-      @groups.last.entries << entry
+      @groups.last.entries << Entry.new(param)
     end
 
     def create_header(name, param)
-      head.send("#{name}=", param) if Header.attributes.include?(name)
+      if Header.attributes.include?(name)
+        head.send("#{name}=", param) 
+      end
     end
      
     def create_entry_item(name,param)
